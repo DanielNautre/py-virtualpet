4c35b3b31d11189d856d7fe76ad6f6cfb51aa5b9
Daniel <daniel.nautre@gmail.com>
2016-05-17 10:38:10 +0200

    Change: improved starter programm slightly

--- starter.py
+++ starter.py
@@ -9,6 +9,7 @@ class starter(Tkinter.Tk):
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
         self.parent = parent
+        self.geometry("300x200")
         self.run()
 
     def run(self):
@@ -17,6 +18,7 @@ class starter(Tkinter.Tk):
         self.nameField.pack()
         createBtn = Tkinter.Button(self, text="Create", command=self.createPet)
         createBtn.pack()
+        self.bind("<return>", self.createPet)
 
     def createPet(self):
         self.name = self.nameField.get()
